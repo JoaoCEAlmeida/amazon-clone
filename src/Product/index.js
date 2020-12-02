414c2dd84548becf8173d50ea2c902c76f2fed65
johnnygato <joao88vsc@gmail.com>
2020-12-02 20:37:18 +0000

    improve layout in small devices

--- src/Product/index.js
+++ src/Product/index.js
@@ -34,7 +34,7 @@ const Product = ({ id, title, image, price, rating }) => {
   return (
     <div className="product">
       <div className="product__info">
-        <p>{title}</p>
+        <p className="product__info__title">{title}</p>
         <p className="product__price">
           <small>$</small>
           <strong>{price}</strong>
